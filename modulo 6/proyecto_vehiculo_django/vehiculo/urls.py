20f5c5449123ebaad585aac86b761f95322a897a
Alvaro Navarrete <115111730+Alvaro-Navarrete@users.noreply.github.com>
2024-12-11 21:50:16 -0300

    Delete modulo 6/proyecto_vehiculo_django directory

--- modulo 6/proyecto_vehiculo_django/vehiculo/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('vehiculo/add/', views.agregar, name='agregar'),
-    path('vehiculo/listado/', views.listar, name='listar'),
-]
